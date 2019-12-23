01561f4026c31c7c2ab12fb22e717629983447d6
MohamedElsaed <ashraf@orcas.io>
2019-12-24 00:51:46 +0200

    code inspection

--- app/Http/Controllers/StatisticsReportController.php
+++ app/Http/Controllers/StatisticsReportController.php
@@ -1,35 +1,44 @@
-<?php
+<?php /** @noinspection PhpUnused */
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
+/**
+ * Class StatisticsReportController
+ * @package App\Http\Controllers
+ */
 class StatisticsReportController extends Controller
 {
-    protected $data;
+    /**
+     * @var array
+     */
+    protected array $data;
 
-    protected function initData()
+    /**
+     *
+     */
+    final public function initData(): string
     {
-        // ...
+        return 'init for data';
     }
 
-    public function getData($format = 'csv')
+    /**
+     * @param string $format
+     * @return null|array|string
+     */
+    final public function getData(string $format = 'csv'): ?array
     {
         switch ($format) {
             case 'csv':
                 $lines = [];
                 foreach ($this->data as $row) {
-                    $lines = implode(",", $row);
+                    $lines = implode(',', $row);
                 }
                 return implode("\n", $lines);
-
             case 'array':
                 return $this->data;
-
             case 'html':
-                $html = '';
                 // format as HTML ...
-                return $html;
+                return 'html';
         }
     }
 }
