d8618f2b52491a58930b87ce548fe3e3da678bd7
MohamedElsaed <ashraf@orcas.io>
2019-12-24 23:09:49 +0200

    add comments

--- app/example/run.php
+++ app/example/run.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * In OOP world polymorphism is a very simplistic translation means same name, different logic.
+ *  This word consists of two greek words: polys which means “many” and morph which means form or shape. In most cases,
+ *  when we replace conditional with polymorphism,
+ *  we deal with a subtype polymorphism.
+ * This type of polymorphism in OOP means the ability to change the behavior of the method by providing a method with the same name in a child class.
+ */
 use App\example\Square;
 use App\example\Triangle;
 
